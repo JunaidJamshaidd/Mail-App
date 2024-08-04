b9f5746a869b216baa814e241006d0cb843e87cd
Junaid Jamshaid <junaidjamshaid775@gmail.com>
2024-08-05 03:36:21 +0500

    Delete src/firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDtB1ed39qheH0h-YdD_E8-pUp3nZCxbQE",
-    authDomain: "clone-e10b0.firebaseapp.com",
-    projectId: "clone-e10b0",
-    storageBucket: "clone-e10b0.appspot.com",
-    messagingSenderId: "15464555037",
-    appId: "1:15464555037:web:ebcb9a2d9239bb29109cf4"
-  };
